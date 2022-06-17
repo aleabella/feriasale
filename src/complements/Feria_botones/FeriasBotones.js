8e2fbf3557214a5c71c50eadfc9cd9f230ebea12
aleabella <luzbella7991@gmail.com>
2022-06-16 21:33:07 -0500

    routes correction for github pages

--- src/complements/Feria_botones/FeriasBotones.js
+++ src/complements/Feria_botones/FeriasBotones.js
@@ -8,15 +8,15 @@ function FeriasBotones(){
 		<h1>Selecciona la Feria</h1>
 
 		<div class="contenedor-botones">
-			<Link to="/ferias-steam/primera"><button class="boton"><span> <img src={LogoTrebol} alt="LogoTrebol" height="90" />Primera Feria </span></button></Link>
+			<Link to="/primera"><button class="boton"><span> <img src={LogoTrebol} alt="LogoTrebol" height="90" />Primera Feria </span></button></Link>
 			
-			<Link to="/ferias-steam/segunda"><button class="boton"><span><img src={LogoTrebol} alt="LogoTrebol" height ="90"/>Segunda Feria</span></button></Link>
+			<Link to="/segunda"><button class="boton"><span><img src={LogoTrebol} alt="LogoTrebol" height ="90"/>Segunda Feria</span></button></Link>
 
-			<Link to="/ferias-steam/tercera"><button class="boton"><span><img src={LogoTrebol} alt="LogoTrebol" height ="90"/>Tercera Feria</span></button></Link>
+			<Link to="/tercera"><button class="boton"><span><img src={LogoTrebol} alt="LogoTrebol" height ="90"/>Tercera Feria</span></button></Link>
 			
-            <Link to="/ferias-steam/cuarta"><button class="boton"><span><img src={LogoTrebol} alt="LogoTrebol"  height ="90"/>Cuarta Feria</span></button></Link>
+            <Link to="/cuarta"><button class="boton"><span><img src={LogoTrebol} alt="LogoTrebol"  height ="90"/>Cuarta Feria</span></button></Link>
 
-			<Link to="/ferias-steam/quinta"><button class="boton"><span><img src={LogoTrebol} alt="LogoTrebol" height ="90" />Quinta Feria</span></button></Link>
+			<Link to="/quinta"><button class="boton"><span><img src={LogoTrebol} alt="LogoTrebol" height ="90" />Quinta Feria</span></button></Link>
 
 				</div>
 			
